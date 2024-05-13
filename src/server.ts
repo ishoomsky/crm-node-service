b80b0b411593c2293452c9fab2784fdc813af606
ishumsk <ivan.shumski@ibm.com>
2024-05-13 17:49:19 +0300

    server: set up socket and mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import mongoose from "mongoose";
 
 const app = express();
 const httpServer = createServer(app);
@@ -9,3 +10,16 @@ const io = new Server(httpServer);
 app.get('/', (rec, res) => {
     res.send("API is UP");
 })
+
+io.on('connection', () => {
+    console.log('io connect');
+});
+
+mongoose.connect('mongodb://localhost:27017/crm')
+    .then(() => {
+        console.log('[MONGO DB] Connected');
+
+        httpServer.listen(4001, () => {
+            console.log('[SERVER] API is listening on port 4001');
+        })
+    });
