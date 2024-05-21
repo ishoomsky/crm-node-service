3a609a27faf832b0be2482376a01cb0af8389278
ishumsk <ivan.shumski@ibm.com>
2024-05-21 18:35:02 +0300

    server: fix auth token (added 'Bearer ')

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -19,7 +19,7 @@ const normalizeUser = (user: UserDocument) => {
         email: user.email,
         username: user.username,
         id: user.id,
-        token,
+        token: `Bearer ${token}`,
     }
 };
 
